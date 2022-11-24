7a510f416ad0c90a259abbcd19316951a757b804
Poooowero <bartek2002@interia.pl>
2022-11-24 14:34:28 +0100

    FileSudokuBoardDao add

---
+++ src/main/java/org/example/FileSudokuBoardDao.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class FileSudokuBoardDao implements Dao<SudokuBoard>{
+    @Override
+    public SudokuBoard read() {
+        return null;
+    }
+
+    @Override
+    public void write(SudokuBoard arg) {
+
+    }
+}
